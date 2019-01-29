ab45c02c6e7a1c13147ab4a09097bd4fc099ff00
Teddy Seyed <t-tesey@microsoft.com>
2019-01-29 13:30:12 -0800

    Initial commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+enum G_RANGE{
+
+    //% block="16g"
+    G_RANGE_16_G          = 0b11,   
+
+    //% block="8g"
+    G_RANGE_8_G           = 0b10,
+
+    //% block="4g"   
+    G_RANGE_4_G           = 0b01,
+
+    //% block="2g"      
+    G_RANGE_2_G           = 0b00 
+}
